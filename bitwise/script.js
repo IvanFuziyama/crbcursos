44ce238da6340843f75e5b85489611be66807c4c
Ivan Fuziyama <IvanSeiji21@gmail.com>
2024-09-30 10:22:28 -0300

    operadores bitwise

---
+++ bitwise/script.js
@@ -0,0 +1,10 @@
+let n1=10//00001010
+let n2=11//00001011
+
+let res1 = n1 & n2
+let res2 = n1 | n2
+let res3 = n1 ^ n2
+
+console.log(res1)// 00001010 = 10
+console.log(res2)// 00001011 = 11
+console.log(res3)// 00000001 = 1
---
+++ bitwise/script2.js
@@ -0,0 +1,7 @@
+let n1=10
+
+let res1 = n1 << 1
+let res2 = n1 >> 1
+
+console.log(res1)
+console.log(res2)
