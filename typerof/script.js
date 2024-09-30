240656dc34596fbe23d293e3db3ef93ce6905196
Ivan Fuziyama <IvanSeiji21@gmail.com>
2024-09-30 11:41:36 -0300

    tipo da variavel

---
+++ typerof/script.js
@@ -0,0 +1,9 @@
+let v1 = 10
+let v2 = "10"
+let v3 = v2 === v1
+let v4 ={nome:"Bruno"}
+
+console.log(`${v1} é ${typeof(v1)}`)
+console.log(`${v2} é ${typeof(v2)}`)
+console.log(`${v3} é ${typeof(v3)}`)
+console.log(`${v4} é ${typeof(v4)}`)
