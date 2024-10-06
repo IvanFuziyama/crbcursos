82cdfa6c6212e170584c5ac57e5e2d4feccc25fc
Ivan Fuziyama <IvanSeiji21@gmail.com>
2024-10-06 17:25:49 -0300

    switch

---
+++ switch/script.js
@@ -0,0 +1,15 @@
+let col=1
+switch(col){
+    case 1:
+        console.log("Primeiro lugar")
+        break
+    case 2:
+        console.log("Segundo Lugar")       
+        break
+    case 3:
+        console.log("Terceiro Lugar")
+        break
+    default:
+        console.log("Não subiu ao pódio")
+        break
+}
